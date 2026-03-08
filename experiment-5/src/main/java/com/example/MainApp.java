77a364807b06b21e1a3abc65da5dc8241a9e9c9f
Pranay Kiran <pranaykiran7876@gmail.com>
2026-03-08 23:30:28 +0530

    Add files via upload

---
+++ experiment-5/src/main/java/com/example/MainApp.java
@@ -0,0 +1,17 @@
+package com.example;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MainApp {
+    public static void main(String[] args) {
+        // Load Spring XML configuration
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring-config.xml");
+
+        // Get the Student bean
+        Student student = (Student) context.getBean("student");
+
+        // Display student details
+        student.display();
+    }
+}
